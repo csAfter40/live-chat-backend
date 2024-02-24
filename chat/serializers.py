b743abcc1f6bee54f9a9c4ad96a7eedac1e3962f
csAfter40 <csafter40@gmail.com>
2024-02-24 17:35:04 +0300

    feat: add full name field

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,8 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(ModelSerializer):
+def capitalize_all(sentence):
+    words = sentence.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return " ".join(capitalized_words)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ["username", "first_name", "last_name", "thumbnail"]
+        fields = ["username", "first_name", "last_name", "full_name", "thumbnail"]
+
+    def get_full_name(self, obj):
+        full_name = f"{obj.first_name} {obj.last_name}"
+        return capitalize_all(full_name)
