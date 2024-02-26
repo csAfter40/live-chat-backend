1e081f817b20e6413e99e5e5dc8a408ebaac25ce
csAfter40 <csafter40@gmail.com>
2024-02-26 14:37:42 +0300

    feat: add signup user serializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -10,6 +10,7 @@ def capitalize_all(sentence):
 
 class UserSerializer(serializers.ModelSerializer):
     full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "full_name", "thumbnail"]
@@ -17,3 +18,25 @@ class UserSerializer(serializers.ModelSerializer):
     def get_full_name(self, obj):
         full_name = f"{obj.first_name} {obj.last_name}"
         return capitalize_all(full_name)
+
+
+class SignUpUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "thumbnail",
+            "password",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
