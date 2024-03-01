0b64005a4d1b6c262d50b192c917d12a66c1c76d
csAfter40 <csafter40@gmail.com>
2024-03-01 17:44:33 +0300

    feat: add Connection model

--- chat/models.py
+++ chat/models.py
@@ -14,4 +14,16 @@ class User(AbstractUser):
     thumbnail = models.ImageField(upload_to=upload_thumbnail, null=True, blank=True)
 
 
-# Create your models here.
+class Connection(models.Model):
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sent_connections"
+    )
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="received_connections"
+    )
+    approved = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.sender.username} -> {self.receiver.username}"
